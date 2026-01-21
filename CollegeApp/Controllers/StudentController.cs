9426a913198b62cfddadbfed6ab9b8b1ff7c66f8
moaaz malek <madhalfrwat@gmail.com>
2026-01-22 01:37:18 +0300

    Add Repository Memory DB

--- CollegeApp/Controllers/StudentController.cs
+++ CollegeApp/Controllers/StudentController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using CollegeApp.Models;
+using CollegeApp.Repository;
 
 namespace CollegeApp.Controller
 {
@@ -7,5 +9,36 @@ namespace CollegeApp.Controller
     [ApiController]
     public class StudentController : ControllerBase
     {
-    }
+        [HttpGet]
+        public IEnumerable<Student> GetStudents()
+        {
+            return CollegeRepository.Students;
+        }
+        [HttpGet("{id:int}")]
+        public Student GetStudentById(int id)
+        {
+
+            return CollegeRepository.Students.FirstOrDefault(s => s.Id == id);
+        }
+        [HttpGet]
+        public Student GetStudentByName(string name)
+        {
+
+            return CollegeRepository.Students.FirstOrDefault(s => s.Name==name);
+        }
+        [HttpDelete("{id:int}")]
+        public IActionResult DeleteStudent(int id)
+        {
+            var student = CollegeRepository.Students.FirstOrDefault(s => s.Id == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            CollegeRepository.Students.Remove(student);
+            return NoContent();
+
+
+
+
+        }
 }
---
+++ CollegeApp/Repository/CollegeRepository.cs
@@ -0,0 +1,10 @@
+﻿using CollegeApp.Models;
+
+namespace CollegeApp.Repository
+{
+    public  static class CollegeRepository
+    {
+        public static List<Student> Students { get; set; } = [
+            ];
+    }
+}
