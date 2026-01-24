411d67f76102369181d6dbaf4b4a075ce3014476
moaaz malek <madhalfrwat@gmail.com>
2026-01-24 23:42:26 +0300

    Modify DBContext

--- CollegeApp/Models/Student.cs
+++ CollegeApp/Models/Student.cs
@@ -5,8 +5,7 @@ namespace CollegeApp.Models
 {
     public class Student
     {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
         public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
